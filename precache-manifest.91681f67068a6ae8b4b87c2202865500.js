6776a8d658ecf40ddc3d52fc25f1292e2e07d0e6
DenisGerasimenko <gerasimenkodenis7@gmail.com>
2020-07-24 18:28:30 +0300

    Updates

---
+++ precache-manifest.91681f67068a6ae8b4b87c2202865500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fc88a70efbd500d02b3c348a22c321",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a0a29e5fe3ff832601ca",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d41e0d5f30386668a65",
+    "url": "/portfolio/static/js/2.80adaeb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.80adaeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a29e5fe3ff832601ca",
+    "url": "/portfolio/static/js/main.e6dc52ba.chunk.js"
+  },
+  {
+    "revision": "803637d1730467fc0e6b",
+    "url": "/portfolio/static/js/runtime-main.525e7e14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
